04c11752b1e1f79fe430acfa1b230baf8c2c06a3
madalinap22 <popescumadalina.gl@gmail.com>
2025-04-19 11:46:49 +0300

    .

--- AVL.c
+++ AVL.c
@@ -1,217 +1,217 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<malloc.h>
-
-
-//AVL - are grad de echilibru si se baga in nod ca int grad
-//echilibrat - nr de niveluri pe st - nr niveluri dr
-
-typedef struct Cazare {
-	unsigned int id;
-	char* hotel;
-	char* client;
-	float suma;
-	int nrCamere; //unsigned char
-}Cazare;
-
-typedef struct Nod {
-	Cazare info;
-	struct Nod* stanga;
-	struct Nod* dreapta;
-	int grad;
-}Nod;
-
-Cazare initializareCazare(unsigned int id, const char* hotel, const char* client, float suma, int nrCamere) {
-	Cazare c;
-	c.id = id;
-	c.hotel = (char*)malloc(sizeof(char) * (strlen(hotel) + 1));
-	strcpy_s(c.hotel, sizeof(char) * (strlen(hotel) + 1), hotel);
-	c.client = (char*)malloc(sizeof(char) * (strlen(client) + 1));
-	strcpy_s(c.client, sizeof(char) * (strlen(client) + 1), client);
-	c.suma = suma;
-	c.nrCamere = nrCamere;
-	return c;
-}
-
-Nod* creareNod(Cazare c, Nod* stanga, Nod* dreapta) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->stanga = stanga;
-	nou->dreapta = dreapta;
-	nou->info = initializareCazare(c.id, c.hotel, c.client, c.suma, c.nrCamere);
-	return nou;
-	//pot sa fac si gradul
-}
-
-//informatia si unde inserez//cheia de inserare = id-ul
-Nod* inserareArbore(Nod* radacina, Cazare c) {
-	//if radacina? intotdeauna ne intrebam daca avem radacina; fara if radacina nua vem recursivitate
-	if (radacina) { //!=NULL
-		if (c.id < radacina->info.id) {
-			radacina->stanga = inserareArbore(radacina->stanga, c);
-			return radacina;
-		}
-		else if (c.id > radacina->info.id) {
-			radacina->dreapta = inserareArbore(radacina->dreapta, c);
-			return radacina;
-		}
-		else {
-			return radacina;
-		}
-	}
-	return creareNod(c, NULL, NULL);
-}
-
-//afisari de obicei de la stanga la dreapta
-//RSD
-void afisarePreordine(Nod* radacina) {
-	if (radacina) {
-		printf("Grad: %d \n%u, %s, %s, %.2f, %d \n", radacina->grad, radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
-		afisarePreordine(radacina->stanga);
-		afisarePreordine(radacina->dreapta);
-	}
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//#include<malloc.h>
 //
+//
+////AVL - are grad de echilibru si se baga in nod ca int grad
+////echilibrat - nr de niveluri pe st - nr niveluri dr
+//
+//typedef struct Cazare {
+//	unsigned int id;
+//	char* hotel;
+//	char* client;
+//	float suma;
+//	int nrCamere; //unsigned char
+//}Cazare;
+//
+//typedef struct Nod {
+//	Cazare info;
+//	struct Nod* stanga;
+//	struct Nod* dreapta;
+//	int grad;
+//}Nod;
+//
+//Cazare initializareCazare(unsigned int id, const char* hotel, const char* client, float suma, int nrCamere) {
+//	Cazare c;
+//	c.id = id;
+//	c.hotel = (char*)malloc(sizeof(char) * (strlen(hotel) + 1));
+//	strcpy_s(c.hotel, sizeof(char) * (strlen(hotel) + 1), hotel);
+//	c.client = (char*)malloc(sizeof(char) * (strlen(client) + 1));
+//	strcpy_s(c.client, sizeof(char) * (strlen(client) + 1), client);
+//	c.suma = suma;
+//	c.nrCamere = nrCamere;
+//	return c;
+//}
+//
+//Nod* creareNod(Cazare c, Nod* stanga, Nod* dreapta) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->stanga = stanga;
+//	nou->dreapta = dreapta;
+//	nou->info = initializareCazare(c.id, c.hotel, c.client, c.suma, c.nrCamere);
+//	return nou;
+//	//pot sa fac si gradul
+//}
+//
+////informatia si unde inserez//cheia de inserare = id-ul
+//Nod* inserareArbore(Nod* radacina, Cazare c) {
+//	//if radacina? intotdeauna ne intrebam daca avem radacina; fara if radacina nua vem recursivitate
+//	if (radacina) { //!=NULL
+//		if (c.id < radacina->info.id) {
+//			radacina->stanga = inserareArbore(radacina->stanga, c);
+//			return radacina;
+//		}
+//		else if (c.id > radacina->info.id) {
+//			radacina->dreapta = inserareArbore(radacina->dreapta, c);
+//			return radacina;
+//		}
+//		else {
+//			return radacina;
+//		}
+//	}
+//	return creareNod(c, NULL, NULL);
+//}
+//
+////afisari de obicei de la stanga la dreapta
+////RSD
 //void afisarePreordine(Nod* radacina) {
-//
 //	if (radacina) {
-//
-//		printf("%u %s %s %.2f \n", radacina->info.id, radacina->info.hotel,
-//			radacina->info.client, radacina->info.suma);
+//		printf("Grad: %d \n%u, %s, %s, %.2f, %d \n", radacina->grad, radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
 //		afisarePreordine(radacina->stanga);
 //		afisarePreordine(radacina->dreapta);
+//	}
+//}
+////
+////void afisarePreordine(Nod* radacina) {
+////
+////	if (radacina) {
+////
+////		printf("%u %s %s %.2f \n", radacina->info.id, radacina->info.hotel,
+////			radacina->info.client, radacina->info.suma);
+////		afisarePreordine(radacina->stanga);
+////		afisarePreordine(radacina->dreapta);
+////
+////	}
+////
+////}
 //
+////SDR
+//void afisarePostordine(Nod* radacina) {
+//	if (radacina) {
+//		afisarePostordine(radacina->stanga);
+//		afisarePostordine(radacina->dreapta);
+//		printf("%u, %s, %s, %.2f, %d \n", radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
+//	}
+//}
+//
+////SRD
+//void afisareInordine(Nod* radacina) {
+//	if (radacina) {
+//		afisareInordine(radacina->stanga);
+//		printf("%u, %s, %s, %.2f, %d \n", radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
+//		afisareInordine(radacina->dreapta);
+//	}
+//}
+//int maxim(int a, int b) {
+//	if (a > b) {
+//		return a;
+//	}
+//	else {
+//		return b;
 //	}
 //
+//	//return a > b ? a : b;
 //}
-
-//SDR
-void afisarePostordine(Nod* radacina) {
-	if (radacina) {
-		afisarePostordine(radacina->stanga);
-		afisarePostordine(radacina->dreapta);
-		printf("%u, %s, %s, %.2f, %d \n", radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
-	}
-}
-
-//SRD
-void afisareInordine(Nod* radacina) {
-	if (radacina) {
-		afisareInordine(radacina->stanga);
-		printf("%u, %s, %s, %.2f, %d \n", radacina->info.id, radacina->info.hotel, radacina->info.client, radacina->info.suma, radacina->info.nrCamere);
-		afisareInordine(radacina->dreapta);
-	}
-}
-int maxim(int a, int b) {
-	if (a > b) {
-		return a;
-	}
-	else {
-		return b;
-	}
-
-	//return a > b ? a : b;
-}
-//nr niveluri - necesara pt calcul nr grad
-int niveluri(Nod* radacina) {
-	if (radacina) {
-		return 1 + maxim(niveluri(radacina->stanga), niveluri(radacina->dreapta)); //1 de la radacina + maxim ce am pe stanga si ce am pe dreapta
-	}
-	else {
-		return 0;
-	}
-}
-
-//functie de calculare a gradului; (neaparat functie de nrNiveluri (inaltime))
-void calculGrad(Nod* rad) {
-	if (rad) {
-		rad->grad = (niveluri(rad->stanga) - niveluri(rad->dreapta));
-		calculGrad(rad->stanga);
-		calculGrad(rad->dreapta);
-	} //fiecare nod sa stie diferenta
-}//se salveaza in campul de grad
-
-//rotiri
-Nod* rotireStanga(Nod* rad) {
-	Nod* x = rad->dreapta;
-	Nod* y = x->dreapta;
-
-	
-	x->stanga = rad; 
-	rad->dreapta = NULL;
-	
-	return x; //devine radacina
-}
-
-Nod* rotireDreapta(Nod* rad) {
-	Nod* x = rad->stanga;
-	Nod* y = x->stanga;
-
-	x->dreapta = rad;
-	rad->stanga = y;
-	return x; //return radacina
-}
-
-Nod* rotireStangaDreapta(Nod* rad){
-	rad->stanga = rotireStanga(rad->stanga);
-	return rotireDreapta(rad);
-
-}
-
-Nod* rotireDreaptaStanga(Nod* rad) {
-	rad->dreapta = rotireDreapta(rad->dreapta);
-	return rotireStanga(rad);
-
-}
-void main() {
-	Cazare c;
-	Nod* radacina = NULL;
-
-	char linieBuffer[100];
-	char separator[3] = ","; //\n
-	char* token;
-
-	FILE* f = fopen("fisierABC.txt", "r");
-	while (fgets(linieBuffer, 100, f)) {
-		token = strtok(linieBuffer, separator);
-		c.id = atoi(token);
-
-		token = strtok(NULL, separator);
-		c.hotel = (char*)malloc(sizeof(char) * (strlen(token) + 1));
-		strcpy_s(c.hotel, sizeof(char) * (strlen(token) + 1), token);
-
-		token = strtok(NULL, separator);
-		c.client = (char*)malloc(sizeof(char) * (strlen(token) + 1));
-		strcpy_s(c.client, sizeof(char) * (strlen(token) + 1), token);
-
-		token = strtok(NULL, separator);
-		c.suma = atof(token);
-
-		token = strtok(NULL, separator);
-		c.nrCamere = atoi(token);
-
-		radacina = inserareArbore(radacina, c);
-
-		free(c.hotel);
-		free(c.client);
-
-	}
-	fclose(f);
-
-	calculGrad(radacina);
-	printf("Afisare initiala (arbore dezechilibrat):\n");
-	afisarePreordine(radacina);
-
-	printf("\nAfisare dupa rotire stanga (echilibrat):\n");
-	radacina = rotireStanga(radacina);
-	calculGrad(radacina);
-	afisarePreordine(radacina);
-
-	//12, Dunarea, Alexandra, 455.5, 6
-	//5, Rin, Patricia, 555.5, 2
-	//	12, Caraiman, Ana Maria, 1555.5, 4
-	//	13, Rin, Amalia, 50.5, 1
-	//	15, Caraiman, Florentina, 555.5, 2
-
-}
+////nr niveluri - necesara pt calcul nr grad
+//int niveluri(Nod* radacina) {
+//	if (radacina) {
+//		return 1 + maxim(niveluri(radacina->stanga), niveluri(radacina->dreapta)); //1 de la radacina + maxim ce am pe stanga si ce am pe dreapta
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+////functie de calculare a gradului; (neaparat functie de nrNiveluri (inaltime))
+//void calculGrad(Nod* rad) {
+//	if (rad) {
+//		rad->grad = (niveluri(rad->stanga) - niveluri(rad->dreapta));
+//		calculGrad(rad->stanga);
+//		calculGrad(rad->dreapta);
+//	} //fiecare nod sa stie diferenta
+//}//se salveaza in campul de grad
+//
+////rotiri
+//Nod* rotireStanga(Nod* rad) {
+//	Nod* x = rad->dreapta;
+//	Nod* y = x->dreapta;
+//
+//	
+//	x->stanga = rad; 
+//	rad->dreapta = NULL;
+//	
+//	return x; //devine radacina
+//}
+//
+//Nod* rotireDreapta(Nod* rad) {
+//	Nod* x = rad->stanga;
+//	Nod* y = x->stanga;
+//
+//	x->dreapta = rad;
+//	rad->stanga = y;
+//	return x; //return radacina
+//}
+//
+//Nod* rotireStangaDreapta(Nod* rad){
+//	rad->stanga = rotireStanga(rad->stanga);
+//	return rotireDreapta(rad);
+//
+//}
+//
+//Nod* rotireDreaptaStanga(Nod* rad) {
+//	rad->dreapta = rotireDreapta(rad->dreapta);
+//	return rotireStanga(rad);
+//
+//}
+//void main() {
+//	Cazare c;
+//	Nod* radacina = NULL;
+//
+//	char linieBuffer[100];
+//	char separator[3] = ","; //\n
+//	char* token;
+//
+//	FILE* f = fopen("fisierABC.txt", "r");
+//	while (fgets(linieBuffer, 100, f)) {
+//		token = strtok(linieBuffer, separator);
+//		c.id = atoi(token);
+//
+//		token = strtok(NULL, separator);
+//		c.hotel = (char*)malloc(sizeof(char) * (strlen(token) + 1));
+//		strcpy_s(c.hotel, sizeof(char) * (strlen(token) + 1), token);
+//
+//		token = strtok(NULL, separator);
+//		c.client = (char*)malloc(sizeof(char) * (strlen(token) + 1));
+//		strcpy_s(c.client, sizeof(char) * (strlen(token) + 1), token);
+//
+//		token = strtok(NULL, separator);
+//		c.suma = atof(token);
+//
+//		token = strtok(NULL, separator);
+//		c.nrCamere = atoi(token);
+//
+//		radacina = inserareArbore(radacina, c);
+//
+//		free(c.hotel);
+//		free(c.client);
+//
+//	}
+//	fclose(f);
+//
+//	calculGrad(radacina);
+//	printf("Afisare initiala (arbore dezechilibrat):\n");
+//	afisarePreordine(radacina);
+//
+//	printf("\nAfisare dupa rotire stanga (echilibrat):\n");
+//	radacina = rotireStanga(radacina);
+//	calculGrad(radacina);
+//	afisarePreordine(radacina);
+//
+//	//12, Dunarea, Alexandra, 455.5, 6
+//	//5, Rin, Patricia, 555.5, 2
+//	//	12, Caraiman, Ana Maria, 1555.5, 4
+//	//	13, Rin, Amalia, 50.5, 1
+//	//	15, Caraiman, Florentina, 555.5, 2
+//
+//}
--- ListaDeListe.c
+++ ListaDeListe.c
@@ -1,182 +1,182 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include<string.h>
-//#include<malloc.h>
-//
-//
-//
-//typedef struct Student Student;
-//typedef struct Nodls Nodls;
-//typedef struct Nodlp Nodlp;
-//
-//typedef struct Student {
-//	unsigned int id;
-//	char* nume;
-//	unsigned char nrMaterii;//0-255 - > este numeric
-//	int* note;
-//} Student;
-//
-//typedef struct Nodls {
-//	Student info;
-//	struct Nodls* next;
-//
-//} Nodls;
-//
-//typedef struct Nodlp {
-//	Nodls* info; //cap de lista secundara
-//	Nodlp* next;
-//
-//} Nodlp;
-//
-//
-//Student initializareStudent(unsigned int id, const char* nume, unsigned char nrMaterii, int* note) {
-//	Student s;
-//	s.id = id;
-//	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-//	strcpy_s(s.nume, sizeof(char) * (strlen(nume) + 1), nume);
-//	s.nrMaterii = nrMaterii;
-//	s.note = (int*)malloc(sizeof(int) * nrMaterii);
-//	for (int i = 0; i < nrMaterii; i++) {
-//		s.note[i] = note[i];
-//	}
-//
-//	return s;
-//}
-//
-////ins in lista simpla/secundara
-//Nodls* inserareLs(Nodls* capls, Student s) {
-//	//creare nod
-//	Nodls* nou = (Nodls*)malloc(sizeof(Nodls));
-//	nou->info = initializareStudent(s.id, s.nume, s.nrMaterii, s.note);
-//	nou->next = NULL;
-//
-//	//inserare
-//	if (capls == NULL) {
-//		capls = nou;
-//	}
-//	else {
-//		Nodls* aux = capls;
-//		while (aux->next) {
-//			aux = aux->next;
-//		}
-//		aux->next = nou;
-//	}
-//	return capls;
-//}
-//
-////inserare lista principala
-//Nodlp* inserareLp(Nodlp* caplp, Nodls* capls) {//adresa de inceput a listei secundare
-//	Nodlp* nou = (Nodlp*)malloc(sizeof(Nodlp));
-//	nou->info = capls; //asteapta o adresa, am o adresa
-//	nou->next = NULL;
-//	if (caplp == NULL) {
-//		caplp = nou;
-//	}
-//	else {
-//		Nodlp* aux = caplp;
-//		while (aux->next) { //while(aux) nu insereaza
-//			aux = aux->next;
-//		}
-//		aux->next = nou;
-//	}
-//	return caplp;
-//}
-////afisare Student
-//void afisareStudent(Student s)
-//{
-//	printf("\n%u, %s, %u   ", s.id, s.nume, s.nrMaterii);
-//	for (int i = 0; i < s.nrMaterii; i++) {
-//		printf("%d ", s.note[i]);
-//	}
-//}
-////afisare ls
-//void afisareLs(Nodls* capls) {
-//	Nodls* aux = capls;
-//	while (aux) {
-//		printf("\n%u, %s, %u   ", aux->info.id, aux->info.nume, aux->info.nrMaterii);
-//		for (int i = 0; i < aux->info.nrMaterii; i++) {
-//			printf("%d ", aux->info.note[i]);
-//		}
-//		aux = aux->next;
-//	}
-//	printf("\n");
-//}
-//
-////afisare lp
-//void afisareLp(Nodlp* caplp) {
-//	Nodlp* aux = caplp;
-//	int contor = 1;
-//	while (aux) {
-//		printf("Sublista: %d", contor++);
-//		afisareLs(aux->info);
-//		aux = aux->next;
-//	}
-//}
-//
-//void dezalocareLs(Nodls* capls) {
-//	Nodls* aux = capls;
-//	while (aux) {
-//		free(aux->info.nume);
-//		free(aux->info.note);
-//		Nodls* temp = aux->next;
-//		free(aux);
-//		aux = temp;
-//	}
-//}
-//
-//void dezalocareLp(Nodlp* caplp) {
-//	Nodlp* aux = caplp;
-//	while (aux) {
-//		dezalocareLs(aux->info);
-//		Nodlp* temp = aux->next;
-//		free(aux);
-//		aux = temp;
-//	}
-//}
-//
-//void main()
-//{
-//	int nr;
-//	Student s;
-//	Nodls* capls = NULL;
-//	Nodlp* caplp = NULL;
-//	Nodls* capls1 = NULL;
-//
-//	char buffer[40];
-//
-//	FILE* f = fopen("fisierListaDeListe.txt", "r");
-//	fscanf(f, "%d", &nr);
-//
-//	for (int i = 0; i < nr; i++) {
-//		fscanf(f, "%u", &s.id);
-//		fscanf(f, "%s", buffer);
-//		s.nume = (char*)malloc(sizeof(char) * (strlen(buffer) + 1));
-//		strcpy_s(s.nume, sizeof(char) * (strlen(buffer) + 1), buffer);
-//		fscanf(f, "%u", &s.nrMaterii);
-//		s.note = (int*)malloc(sizeof(int) * s.nrMaterii);
-//		for (int i = 0; i < s.nrMaterii; i++) {
-//			fscanf(f, "%d", &s.note[i]);
-//		}
-//
-//		//conditii
-//		if (s.id % 2 == 0) {
-//			capls = inserareLs(capls, s);
-//		}
-//		else {
-//			capls1 = inserareLs(capls1, s);
-//		}
-//
-//		free(s.nume);
-//		free(s.note);
-//	}
-//	fclose(f);
-//	afisareLs(capls);
-//	afisareLs(capls1);
-//
-//	caplp = inserareLp(caplp, capls);
-//	caplp = inserareLp(caplp, capls1);
-//
-//	printf("\nLista de liste ------------------------------\n");
-//	afisareLp(caplp);
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<malloc.h>
+
+
+
+typedef struct Student Student;
+typedef struct Nodls Nodls;
+typedef struct Nodlp Nodlp;
+
+typedef struct Student {
+	unsigned int id;
+	char* nume;
+	unsigned char nrMaterii;//0-255 - > este numeric
+	int* note;
+} Student;
+
+typedef struct Nodls {
+	Student info;
+	struct Nodls* next;
+
+} Nodls;
+
+typedef struct Nodlp {
+	Nodls* info; //cap de lista secundara
+	Nodlp* next;
+
+} Nodlp;
+
+
+Student initializareStudent(unsigned int id, const char* nume, unsigned char nrMaterii, int* note) {
+	Student s;
+	s.id = id;
+	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+	strcpy_s(s.nume, sizeof(char) * (strlen(nume) + 1), nume);
+	s.nrMaterii = nrMaterii;
+	s.note = (int*)malloc(sizeof(int) * nrMaterii);
+	for (int i = 0; i < nrMaterii; i++) {
+		s.note[i] = note[i];
+	}
+
+	return s;
+}
+
+//ins in lista simpla/secundara
+Nodls* inserareLs(Nodls* capls, Student s) {
+	//creare nod
+	Nodls* nou = (Nodls*)malloc(sizeof(Nodls));
+	nou->info = initializareStudent(s.id, s.nume, s.nrMaterii, s.note);
+	nou->next = NULL;
+
+	//inserare
+	if (capls == NULL) {
+		capls = nou;
+	}
+	else {
+		Nodls* aux = capls;
+		while (aux->next) {
+			aux = aux->next;
+		}
+		aux->next = nou;
+	}
+	return capls;
+}
+
+//inserare lista principala
+Nodlp* inserareLp(Nodlp* caplp, Nodls* capls) {//adresa de inceput a listei secundare
+	Nodlp* nou = (Nodlp*)malloc(sizeof(Nodlp));
+	nou->info = capls; //asteapta o adresa, am o adresa
+	nou->next = NULL;
+	if (caplp == NULL) {
+		caplp = nou;
+	}
+	else {
+		Nodlp* aux = caplp;
+		while (aux->next) { //while(aux) nu insereaza
+			aux = aux->next;
+		}
+		aux->next = nou;
+	}
+	return caplp;
+}
+//afisare Student
+void afisareStudent(Student s)
+{
+	printf("\n%u, %s, %u   ", s.id, s.nume, s.nrMaterii);
+	for (int i = 0; i < s.nrMaterii; i++) {
+		printf("%d ", s.note[i]);
+	}
+}
+//afisare ls
+void afisareLs(Nodls* capls) {
+	Nodls* aux = capls;
+	while (aux) {
+		printf("\n%u, %s, %u   ", aux->info.id, aux->info.nume, aux->info.nrMaterii);
+		for (int i = 0; i < aux->info.nrMaterii; i++) {
+			printf("%d ", aux->info.note[i]);
+		}
+		aux = aux->next;
+	}
+	printf("\n");
+}
+
+//afisare lp
+void afisareLp(Nodlp* caplp) {
+	Nodlp* aux = caplp;
+	int contor = 1;
+	while (aux) {
+		printf("Sublista: %d", contor++);
+		afisareLs(aux->info);
+		aux = aux->next;
+	}
+}
+
+void dezalocareLs(Nodls* capls) {
+	Nodls* aux = capls;
+	while (aux) {
+		free(aux->info.nume);
+		free(aux->info.note);
+		Nodls* temp = aux->next;
+		free(aux);
+		aux = temp;
+	}
+}
+
+void dezalocareLp(Nodlp* caplp) {
+	Nodlp* aux = caplp;
+	while (aux) {
+		dezalocareLs(aux->info);
+		Nodlp* temp = aux->next;
+		free(aux);
+		aux = temp;
+	}
+}
+
+void main()
+{
+	int nr;
+	Student s;
+	Nodls* capls = NULL;
+	Nodlp* caplp = NULL;
+	Nodls* capls1 = NULL;
+
+	char buffer[40];
+
+	FILE* f = fopen("fisierListaDeListe.txt", "r");
+	fscanf(f, "%d", &nr);
+
+	for (int i = 0; i < nr; i++) {
+		fscanf(f, "%u", &s.id);
+		fscanf(f, "%s", buffer);
+		s.nume = (char*)malloc(sizeof(char) * (strlen(buffer) + 1));
+		strcpy_s(s.nume, sizeof(char) * (strlen(buffer) + 1), buffer);
+		fscanf(f, "%u", &s.nrMaterii);
+		s.note = (int*)malloc(sizeof(int) * s.nrMaterii);
+		for (int i = 0; i < s.nrMaterii; i++) {
+			fscanf(f, "%d", &s.note[i]);
+		}
+
+		//conditii
+		if (s.id % 2 == 0) {
+			capls = inserareLs(capls, s);
+		}
+		else {
+			capls1 = inserareLs(capls1, s);
+		}
+
+		free(s.nume);
+		free(s.note);
+	}
+	fclose(f);
+	afisareLs(capls);
+	afisareLs(capls1);
+
+	caplp = inserareLp(caplp, capls);
+	caplp = inserareLp(caplp, capls1);
+
+	printf("\nLista de liste ------------------------------\n");
+	afisareLp(caplp);
+}
